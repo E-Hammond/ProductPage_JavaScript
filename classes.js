1b15a2dd3aa49e6d0c39c60a9fead21c4ccd806c
E-Hammond <emmanuel.hammond@amalitech.org>
2021-04-13 23:49:32 +0000

    Deleted app.js and class.js

--- classes.js
+++ classes.js
@@ -60,10 +60,13 @@ class ProductItems extends ProductPage{
             <div class = "product-details">
                 <h2 class="product-title">${this.title}</h2>
                 <p class="description">${this.description}</p>
-                <p class="price">${this.price}</p>
-                <p class="stock">Availabilty : ${this.stock}</p>
-                <button class="addToCart">Add to Cart</button>
-                <button class="removeFromCart">Remove</button>
+                <p>
+                <span>GHS :</span><span class="price"> ${this.price}</span>
+<p>
+                <span>Availability : </span> <span id="stock">${this.stock}</span>
+                <p>
+                <button id = "addToCart">Add to Cart</button>
+                <button id="removeFromCart">Remove</button>
             </div>
         </div>
         
@@ -77,66 +80,81 @@ class ProductItems extends ProductPage{
         let sectionCard = document.createElement("div");
         sectionCard.className = "sectionCard";
         sectionCard.innerHTML = `
-        <span id="cartSection">Cart Section</span>
+        <span id="cartSection">Add To Cart</span>
         <button id="cartSectionNumber">0</button>
-        <div class="cartDetailsTitle">
-            <div class="cartColumn">PRODUCT ITEMS</div>
-            <div class="cartColumn">QUANTITY</div>
-            <div class="cartColumn">UNIT PRICE</div>
-            <div class="cartColumn">TOTAL</div>
-        </div>
-        <div class="cartDetails">
-            <div class="cartDetailsColumn">
-                <div id="cartDetailsColumn1_div1">Not items added yet</div>
-                <div id="cartDetailsColumn1_div2"></div>
-                <div id="cartDetailsColumn1_div3"></div>
-                <div id="cartDetailsColumn1_div4"></div>
-                <div id="cartDetailsColumn1_div5"></div>
-            </div>
-            <div class="cartDetailsColumn">
-                <div id="cartDetailsColumn2_div1">NULL</div>
-                <div id="cartDetailsColumn2_div2"></div>
-                <div id="cartDetailsColumn2_div3"></div>
-                <div id="cartDetailsColumn2_div4"></div>
-                <div id="cartDetailsColumn2_div5"></div>
-            </div>
-            <div class="cartDetailsColumn">
-                <div id="cartDetailsColumn3_div1">NULL</div>
-                <div id="cartDetailsColumn3_div2"></div>
-                <div id="cartDetailsColumn3_div3"></div>
-                <div id="cartDetailsColumn3_div4"></div>
-                <div id="cartDetailsColumn3_div5"></div>
-            </div>
-            <div class="cartDetailsColumn">
-                <div id="cartDetailsColumn4_div1">NULL</div>
-                <div id="cartDetailsColumn4_div2"></div>
-                <div id="cartDetailsColumn4_div3"></div>
-                <div id="cartDetailsColumn4_div4"></div>
-                <div id="cartDetailsColumn4_div5"></div>
-            </div>
-        </div>
+        <hr style="margin-top:15px;background-color:tomato"><hr style="background-color:red;">
+        
         `
         return sectionCard;
 
     }
 
+
+
 }
 
 const products = [
-    new ProductItems("Mario Fagni Hand Weaved Black Sneaker",100,2,
+    new ProductItems("Mario Fagni Hand Weaved Black Sneaker",100,1,
     "This sneaker is made from hand weaved calfskin leather and has an additional top leather accessories  on top of a plain leather that beautifies it.",
     "Images/boot.jpg"),
-    new ProductItems("Plain And Patterned Calfskin White Nappa",150.00,4,"This shoe is made from Plain and Patterned calfskin leather.The shoe has an expandable underneath the top cover that accommodate high Upper feet",
+    new ProductItems("Plain And Patterned Calfskin White Nappa",150.00,1,"This shoe is made from Plain and Patterned calfskin leather.The shoe has an expandable underneath the top cover that accommodate high Upper feet",
 "Images/boot1.webp"),
-    new ProductItems("Rossi Toehold Sneaker - Black",250.00,4,"This Sneaker is made from Authentic Calf Skin Leather. It has a Leather Sole with additional rubber mid fit  for a firm grip of the slippers on the floor",
+    new ProductItems("Rossi Toehold Sneaker - Black",250.00,1,"This Sneaker is made from Authentic Calf Skin Leather. It has a Leather Sole with additional rubber mid fit  for a firm grip of the slippers on the floor",
 "Images/boot3.webp"),
     new ProductItems("Benter Black Tassel Sneaker",120.00,1,"BENTER are known for the production of high quality footwear at affordable price. This shoe has a double sole which makes it durable and long lasting.",
 "Images/boot2.jpg")
 ]
 
+const prod = new ProductItems();
+divContainer.append(prod.cartSection())
+
 products.map(product => {
     divContainer.append(product.rowCard());
 })
 
-const prod = new ProductItems();
-divContainer.append(prod.cartSection())
+
+// prod.addToCart();
+
+
+
+const addCart = document.querySelectorAll("#addToCart");
+const quantity = document.querySelector("#cartSectionNumber")
+const stock = document.querySelector("#stock")
+
+for(const  product of addCart){
+    product.addEventListener("click", function(){
+        if(quantity.textContent == 4){
+            alert("You can no longer add to Cart")
+        }
+
+        else if(!(Number(stock.textContent)<= 0)){
+            // stock.textContent = Numberstock.textContent -1;
+            quantity.textContent = Number(quantity.textContent)+1;
+            stock.textContent = Number(stock.textContent) -1;
+            
+        }
+    })
+}
+
+
+
+const removeCart = document.querySelectorAll("#removeFromCart");
+const quantityy = document.querySelector("#cartSectionNumber")
+const stockk = document.querySelector("#stock")
+
+for(const  product of addCart){
+    product.addEventListener("click", function(){
+        if(quantityy.textContent == 4){
+            alert("You can no longer add to Cart")
+        }
+
+        else if(!(Number(stockk.textContent)<=0)){
+            // stock.textContent = Numberstock.textContent -1;
+            quantityy.textContent = Number(quantityy.textContent)-1;
+            stockk.textContent = Number(stockk.textContent) +1;
+            
+            
+        }
+    })
+}
+
